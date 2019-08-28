152321f206839e37d451f92a427424095e1eeb57
Darin Webster <darinleenavy@gmail.com>
2019-08-28 10:14:56 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,3 +7,4 @@
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [  {name: "Steven", age: "30", attitude: "confused"},
