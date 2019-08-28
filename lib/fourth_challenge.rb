e99d3397fbf2a2c031e7cbfe782f996bdccff7c8
Darin Webster <darinleenavy@gmail.com>
2019-08-28 10:15:56 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,4 +9,7 @@
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [  {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
-    
+      ]
+    }
+  }
+end
