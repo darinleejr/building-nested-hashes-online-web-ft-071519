f61bf11188165b62c4afea19d2f226ed7114ebc3
Darin Webster <darinleenavy@gmail.com>
2019-08-28 10:06:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,8 +19,9 @@ def third_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :patriarch => {	        :heroine_friends => [
-        #your key/value pairs here	            {name: "Steven", age: "30", attitude: "confused"},
+      :patriarch => {	        
+      :heroine_friends =>
+      {name: "Steven", age: "30", attitude: "confused"},
         },	            {name: "Nurse", age: "44", attitude: "worried"}
       :matriarch => {	        ]
         #your key/value pairs here	     }
