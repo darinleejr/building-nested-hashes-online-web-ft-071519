7444dbb657fc48881dd5615cbc0f20a55136ff8e
Darin Webster <darinleenavy@gmail.com>
2019-08-28 06:05:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,15 @@
 def first_challenge
    epic_tragedy = {}
    :montague => {
-#       :patriarch => {
-#         :name => "Lord Montague",
-#         :age => "53"
-#       },
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
    :capulet => {
-#       :patriarch => {
-#         :name => "Lord Capulet"
-#         :age => "50"
-#       }
+      :patriarch => {
+        :name => "Lord Capulet"
+        :age => "50"
+      }
    }
 end
 
