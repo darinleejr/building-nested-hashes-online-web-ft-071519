89b9d0c4866b8b13ea215a2a2301e4597f65d177
Darin Webster <darinleenavy@gmail.com>
2019-08-28 10:16:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,3 +1,5 @@
+def first_challenge
+   epic_tragedy = {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
