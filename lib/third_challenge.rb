f0061ccf45658f0551b5c1a53c6d0ae760424d49
Darin Webster <darinleenavy@gmail.com>
2019-08-28 10:09:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,10 +6,10 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :matriarch => {
       :hero_friends => [ 
-      {name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-      
+    },  
       
     :capulet => {
       :hero_friends => []
